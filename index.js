4dd22061da08f6de2ccfca9e633f3ae46fd1be32
olegchursin <olegchursin@gmail.com>
2018-02-06 09:34:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+//$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+
+const honestAbe = document.querySelector('#save_lincoln')
+honestAbe.addEventListener('click', function(event) {
+  // const intervalValue = document.querySelector('#interval').value
+  // saveLincoln(parseInt(intervalValue))
+  saveLincoln(parseInt(document.querySelector('#interval').value))
+})
+
 
 function saveLincoln(interval) {
   if(isNaN(interval)) {
     interval = 10000
   }
-  $('#foreground').fadeOut(interval);
+  var fg = document.querySelector("#foreground");
+    var fadeEffect = setInterval(function () {
+        if (!fg.style.opacity) {
+            fg.style.opacity = 1;
+        }
+        if (fg.style.opacity < 0.1) {
+            clearInterval(fadeEffect);
+        } else {
+            fg.style.opacity -= 0.1;
+        }
+    }, interval);
 }
